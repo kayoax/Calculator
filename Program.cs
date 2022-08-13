01cd1d7c4134a358f9b63aa02be0a9db53f33124
kayoax <62783466+kayoax@users.noreply.github.com>
2022-08-12 22:58:24 -0300

    modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -16,27 +16,62 @@ static void Menu()
   Console.WriteLine("5 - Sair");
   Console.WriteLine("_______________________________________________ \n");
   Console.Write("Digite a opção desejada: ");
-  short opcao = short.Parse(Console.ReadLine());
-  if (opcao == 5) Environment.Exit(0);
+
+  Globals.opcao = Console.ReadLine();
+  while (Globals.opcao != "1"
+  && Globals.opcao != "2"
+  && Globals.opcao != "3"
+  && Globals.opcao != "4"
+  && Globals.opcao != "5")
+  {
+    Console.Clear();
+    Console.WriteLine("Opção inválida!");
+    Console.Write("Digite a opção desejada: ");
+    Globals.opcao = Console.ReadLine();
+  }
+  if (Globals.opcao == "5")
+  {
+    Console.WriteLine("Obrigado por utilizar o Kalculator!");
+    Environment.Exit(0);
+  }
+  else
+  {
+    Calculadora();
+  }
+}
+
+static void Calculadora()
+{
 
   Console.Clear();
   Console.WriteLine("_______________________________________________ \n");
-  Console.WriteLine("Digite os dois números que deseja calcular: ");
-
+  Console.WriteLine("Digite os dois números que deseja calcular: \n");
   Console.Write("Digite o primeiro número: ");
   float num1 = float.Parse(Console.ReadLine());
+
+  //verifica se é um numero ou letra
+  while (!float.TryParse(num1.ToString(), out num1))
+  {
+    Console.Clear();
+    Console.WriteLine("Digite um número válido!");
+    Console.Write("Digite o primeiro número: ");
+    num1 = float.Parse(Console.ReadLine());
+  }
+
+
+
   Console.Write("Digite o segundo número: ");
   float num2 = float.Parse(Console.ReadLine());
   Console.WriteLine("_______________________________________________ \n");
   Console.WriteLine("Processando...: ");
   Console.Clear();
 
-  switch (opcao)
+  switch (Globals.opcao)
   {
-    case 1: Soma(num1, num2); break;
-    case 2: Subtracao(num1, num2); break;
-    case 3: Multiplicacao(num1, num2); break;
-    case 4: Divisao(num1, num2); break;
+    case "1": Soma(num1, num2); break;
+    case "2": Subtracao(num1, num2); break;
+    case "3": Multiplicacao(num1, num2); break;
+    case "4": Divisao(num1, num2); break;
     default:
       Console.WriteLine("Opção inválida!");
       Menu(); break;
@@ -67,4 +102,8 @@ static void Multiplicacao(float num1, float num2)
 {
   var result = num1 * num2;
   Console.WriteLine($"O Resultado da Multiplicação é: {result}");
+}
+public static class Globals
+{
+  public static string opcao;
 }
